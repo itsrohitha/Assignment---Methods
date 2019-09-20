d63e3d30089ed9c2ea8156d2715f89a3043f8f84
Naga Rohitha Nannapaneni <nagarohitha@usf.edu>
2019-09-19 21:20:19 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Class_Project___Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetUser ();
+            Console.ReadKey();
+            
+        }
+
+        public static void GreetUser()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello " + input + "!");
+        }
+    }
+}
